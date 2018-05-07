64a3ba3415e0c9019996471f7fcbdd6231baddf7
kneric <brian.pradipta.a@gmail.com>
2018-05-07 11:10:56 +0700

    a better understanding of splice

--- mengelompokkanAngka.js
+++ mengelompokkanAngka.js
@@ -7,16 +7,16 @@ function mengelompokkanAngka(arr) {
   var result =[];
   for (var i = 0; i < arr.length; i++){
     if (arr[i] % 3 === 0){
-      a = arr.splice(arr[i],1)
+      a = arr.splice(i,1, arr[i])
       tampung.push (a);
     } else if (arr[i] % 2 === 0){
       genap.push(arr[i]);
     } else {
       ganjil.push(arr[i]);
     }
-    console.log (i, genap)
-    console.log (i, ganjil)
-    console.log (i, tampung)
+    // console.log (i, genap)
+    // console.log (i, ganjil)
+    // console.log (i, tampung)
   }
   if (tampung != []){
     var kelipatan3 = [];
@@ -32,6 +32,6 @@ function mengelompokkanAngka(arr) {
 
 // TEST CASES
 console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
-// console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
-// console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
-// console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
